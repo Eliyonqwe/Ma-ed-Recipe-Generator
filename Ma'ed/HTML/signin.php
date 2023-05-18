7700b27651ae0f7d8d0436023916c5207282b38a
Michyas20 <101133166+Michyas20@users.noreply.github.com>
2023-05-18 13:16:22 +0300

    Login & Signup php
    sql copy included

--- Ma'ed/HTML/signin.php
+++ Ma'ed/HTML/signin.php
@@ -1,15 +1,16 @@
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>signin</title>
   <link rel="stylesheet" href="../styles/signin.css">
   <script src="https://kit.fontawesome.com/a837bf0db4.js" crossorigin="anonymous"></script>
 </head>
 <body>
-  <div class="item1">  <a href="login.html"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
+  <div class="item1">  <a href="login.php"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
     <div class="logo">
       <a href="home.html"><img src="../pics/Maed_logo1-removebg.png" alt="" /></a>
      </div>
@@ -20,12 +21,13 @@
 
     <div class="item13">
       
-      <form action="#" class="i1">
+      <form action="../HTML/signin.php" method="post" class="i1">
+        <?php include('errors.php'); ?>
         <img src="../pics/256-512.webp" alt="Avatar" class="avatar">
         <h1>Sign In</h1>
         <p>Enter your Email address <br>and password to LogIn</p>
-        <input type="email" placeholder="  Email" />
-        <input type="password" placeholder=" Password" />
+        <input type="text" placeholder="   username" name="username" />
+        <input type="password" placeholder=" Password" name="password" />
        <a href="home.html"> <button>Sign In</button> </a>
        <input type="checkbox" checked="checked" name="remeber">Remember Me
     </form>
---
+++ Ma'ed/HTML/signup.php
@@ -0,0 +1,67 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>signup</title>
+    <link rel="stylesheet" href="../styles/signup.css" />
+    <script
+      src="https://kit.fontawesome.com/a837bf0db4.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="item1">
+        <a href="home.html"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
+      </div>
+      <div class="item2"></div>
+
+      <div class="item3">
+        <div class="logo">
+          <a href="home.html"
+            ><img src="../pics/Maed_logo1-removebg.png" alt=""
+          /></a>
+        </div>
+      </div>
+
+      <div class="item4">
+        <form action="../HTML/signup.php" method="post"   >
+          <?php include('errors.php');?>
+          <h4>Create an Account</h4>
+          <div class="icon-login">
+            <a href="#" class="icon"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="icon"><i class="fab fa-google-plus-g"></i></a>
+            <a href="#" class="icon"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+          <p>
+            Enter your personal details <br />
+            to Resgister
+          </p>
+
+          <input type="text" placeholder="Username" name="username" value="<?php echo $username; ?>"/>
+          <input type="email" placeholder="  Email" name="email" value="<?php echo $email; ?>" />
+          <input type="password" placeholder=" Password" name="password_1" />
+          <input type="password" placeholder="Repeat Password" name="password_2"/>
+          <button type="submit" name="reg_user">Sign Up</button>
+        </form>
+      </div>
+
+      <div class="item5">
+        <h1>
+          Welcome to Ma'ed. <br />
+          The First Recipe Generator Site in Ethiopia.
+        </h1>
+        <p>Sign In your Account and start journey with us</p>
+        <a href="signin.php"><button>Sign In</button></a>
+      </div>
+
+      <div class="item6"></div>
+
+      <div class="item7"></div>
+      <div class="item8"></div>
+    </div>
+  </body>
+</html>
