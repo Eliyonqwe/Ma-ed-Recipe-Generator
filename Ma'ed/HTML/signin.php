e70e2a09e7035584157ab58529fe94011dc38e07
eliyon <55352187+Eliyonqwe@users.noreply.github.com>
2023-05-21 16:11:19 +0300

    signin ui edited

--- Ma'ed/HTML/signin.php
+++ Ma'ed/HTML/signin.php
@@ -1,6 +1,7 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,35 +10,37 @@
   <link rel="stylesheet" href="../styles/signin.css">
   <script src="https://kit.fontawesome.com/a837bf0db4.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-  <div class="item1">  <a href="signup.php"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
+  <div class="item1"> <a href="signup.php"><i class="fa-sharp fa-solid fa-arrow-left"></i></a>
     <div class="logo">
       <a href="home.php"><img src="../pics/Maed_logo1-removebg.png" alt="" /></a>
-     </div>
     </div>
-  
+  </div>
+
   <div class="containers555">
 
 
     <div class="item13">
-      
+
       <form action="../HTML/signin.php" method="post" class="i1">
         <?php include('errors.php'); ?>
         <img src="../pics/256-512.webp" alt="Avatar" class="avatar">
         <h1>Sign In</h1>
-        <p>Enter your Email address <br>and password to LogIn</p>
-        <input type="text" placeholder="   username" name="username" />
-        <input type="password" placeholder=" Password" name="password" />
-        <button name="signin_user">Sign In</button>
-       <input type="checkbox" checked="checked" name="remeber">Remember Me
-       <p class="new">create new account!! <br>  <span><a href="signup.php">CLICK HERE!!!</a></span></p>
-    </form>
-       
-      
-     
-</div>
+        <p>Enter your personal information</p>
+        <input type="text" placeholder="Username" name="username" />
+        <input type="password" placeholder="Password" name="password" />
+        <button name="signin_user" class="signin">Sign In</button>
+        <input type="checkbox" checked="checked" name="remeber">Remember Me
+        <p class="new">create new account!! <br> <span><a href="signup.php">CLICK HERE!!!</a></span></p>
+      </form>
+
+
 
     </div>
-  
+
+  </div>
+
 </body>
+
 </html>
