5043d050cb3b9f44d423996d284aef4dbcccf1f8
leuldawit <97153381+leuldawit@users.noreply.github.com>
2023-05-24 20:07:48 +0300

    responsive layout

--- Ma'ed/HTML/home.php
+++ Ma'ed/HTML/home.php
@@ -9,10 +9,11 @@
   <link rel="icon" type="image/x-icon" href="../pics/favicon.ico">
   <link rel="stylesheet" href="../Styles/home.css" />
   <link href="https://fonts.cdnfonts.com/css/proxima-nova-2" rel="stylesheet" />
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
- <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -43,17 +44,32 @@
               <span class="material-symbols-outlined">help</span>FAQs</a>
           </li>
           <li id="separator"><a href="#">|</a></li>
+          <?php
+          session_start();
+          if (isset($_SESSION['username'])) {
+            ?>
+            <li class="phone" id="normal">
+              <a href="#"><span class="material-symbols-outlined ">Home</span>Account</a>
+            </li>
+            <li class="phone" id="normal">
+              <a href="#"><span class="material-symbols-outlined ">Star</span>Favorites</a>
+            </li>
+            <li class="phone" id="normal">
+              <a href="logout.php"><span class="material-symbols-outlined ">logout</span>Sign out</a>
+            </li>
+            <?php
+          }
+          ?>
           <!--           
             <li id="signup">
               <a href="#"><span class="material-symbols-outlined">help</span>Sign Up </a>
             </li> -->
           <li id="account">
             <?php
-            session_start();
 
             if (isset($_SESSION['username'])) {
               $username = $_SESSION['username'];
-            ?>
+              ?>
               <div id="pic" class="pic"><img src="../pics/photo_2021-05-31_08-56-23.jpg" alt=""></div>
               <div class="profile">
                 <ul id="profile">
@@ -69,13 +85,13 @@
                 </ul>
               </div>
 
-            <?php
+              <?php
             } else {
-            ?>
+              ?>
               <a href="signin.php"><button class="account">
                   <span class="material-symbols-outlined">person</span>Account
                 </button></a>
-            <?php
+              <?php
             }
             ?>
           </li>
@@ -193,11 +209,17 @@
         </p>
         <br />
 
-        <a href="/" target="_blank" tabIndex="0"><img class="bn45" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" alt="Google Playstore" /></a>
+        <a href="/" target="_blank" tabIndex="0"><img class="bn45"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png"
+            alt="Google Playstore" /></a>
         <!-- <br> -->
-        <a href="/" target="_blank" tabIndex="0"><img class="bn46" src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="App Store" /></a>
+        <a href="/" target="_blank" tabIndex="0"><img class="bn46"
+            src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+            alt="App Store" /></a>
         <!-- <br> -->
-        <a href='/' target="_blank"><img class="bn44" src='https://developer.microsoft.com/en-us/store/badges/images/English_get-it-from-MS.png' alt='Badge' /></a>
+        <a href='/' target="_blank"><img class="bn44"
+            src='https://developer.microsoft.com/en-us/store/badges/images/English_get-it-from-MS.png'
+            alt='Badge' /></a>
       </div>
     </div>
 
@@ -207,7 +229,7 @@
       <div class="content">
         <div class="top">
           <div class="logo-details">
-            
+
             <span class="logo_name">Ma'ed</span>
           </div>
           <div class="media-icons">
@@ -226,7 +248,7 @@
             <li><a href="#">About us</a></li>
             <li><a href="#">Get started</a></li>
           </ul>
-          
+
           <ul class="box input-box">
             <li class="link_name">Subscribe</li>
             <li><input type="text" placeholder="Enter your email"></li>
