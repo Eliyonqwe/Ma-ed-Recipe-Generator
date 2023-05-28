df970f5ba0c52500c8d33d6d0896cefd1d1d4fc5
Abraham Damtew <abraha1819@gmail.com>
2023-05-28 13:39:32 +0300

    admin page css and js update

--- Ma'ed/Scripts/adminpage.js
+++ Ma'ed/Scripts/adminpage.js
@@ -1,33 +1,111 @@
 $('#addfood').click(function() {
     $('#addFoodPage').show();
     $('#addfood').css('background-color', 'white');
+
+    $('#edit-removefoodpage').hide();
     $('#addIngredientsPage').hide();
+    $('#edit-remove-ing-page').hide();
     $('#usersPage').hide();
-    $('#adding').css('background-color', 'orange');
-    $('#adding').css('color', 'black');
+
+    $('#edit-remove-food').css('background-color', 'orange');
+    $('#edit-remove-food').css('color', 'black');
+
+    $('#adding-ing').css('background-color', 'orange');
+    $('#adding-ing').css('color', 'black');
+
+    $('#edit-remove-ing').css('background-color', 'orange');
+    $('#edit-remove-ing').css('color', 'black');
+    
+    $('#users').css('background-color', 'orange');
+    $('#users').css('color', 'black');
+});
+$('#edit-remove-food').click(function() {
+    $('#edit-removefoodpage').show();
+    $('#edit-remove-food').css('background-color', 'white');
+
+    $('#addFoodPage').hide();
+    $('#addIngredientsPage').hide();
+    $('#edit-remove-ing-page').hide();
+    $('#usersPage').hide();
+
+    $('#addfood').css('background-color', 'orange');
+    $('#addfood').css('color', 'black');
+
+    $('#adding-ing').css('background-color', 'orange');
+    $('#adding-ing').css('color', 'black');
+
+    
+    $('#edit-remove-ing').css('background-color', 'orange');
+    $('#edit-remove-ing').css('color', 'black');
     
     $('#users').css('background-color', 'orange');
     $('#users').css('color', 'black');
 });
 
-$('#adding').click(function() {
+
+$('#adding-ing').click(function() {
+    
     $('#addIngredientsPage').show();
-    $('#adding').css('background-color', 'white');
+    $('#adding-ing').css('background-color', 'white');
+
     $('#addFoodPage').hide();
+    $('#edit-removefoodpage').hide();
+    $('#edit-remove-ing-page').hide();
     $('#usersPage').hide();
+
+    $('#edit-remove-food').css('background-color', 'orange');
+    $('#edit-remove-food').css('color', 'black');
+
     $('#addfood').css('background-color', 'orange');
     $('#addfood').css('color', 'black');
+
+    $('#edit-remove-ing').css('background-color', 'orange');
+    $('#edit-remove-ing').css('color', 'black');
+
     $('#users').css('background-color', 'orange');
     $('#users').css('color', 'black');
 });
+$('#edit-remove-ing').click(function() {
+    $('#edit-remove-ing-page').show();
+    $('#edit-remove-ing').css('background-color', 'white');
+
+    $('#addFoodPage').hide();
+    $('#addIngredientsPage').hide();
+    $('#edit-removefoodpage').hide();
+    $('#usersPage').hide();
+
+    $('#edit-remove-food').css('background-color', 'orange');
+    $('#edit-remove-food').css('color', 'black');
+
+    $('#addfood').css('background-color', 'orange');
+    $('#addfood').css('color', 'black');
+
+    $('#adding-ing').css('background-color', 'orange');
+    $('#adding-ing').css('color', 'black');
+
+    $('#users').css('background-color', 'orange');
+    $('#users').css('color', 'black');
+})
 
 $('#users').click(function() {
     $('#usersPage').show();
     $('#users').css('background-color', 'white');
+
     $('#addFoodPage').hide();
+    $('#edit-removefoodpage').hide();
     $('#addIngredientsPage').hide();
+    $('#edit-remove-ing-page').hide();
+
+    $('#edit-remove-food').css('background-color', 'orange');
+    $('#edit-remove-food').css('color', 'black');
+
     $('#addfood').css('background-color', 'orange');
     $('#addfood').css('color', 'black');
-    $('#adding').css('background-color', 'orange');
-    $('#adding').css('color', 'black');
+
+    $('#adding-ing').css('background-color', 'orange');
+    $('#adding-ing').css('color', 'black');
+
+    $('#edit-remove-ing').css('background-color', 'orange');
+    $('#edit-remove-ing').css('color', 'black');
 });
+
